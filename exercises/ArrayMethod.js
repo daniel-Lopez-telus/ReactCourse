4d08116199b3b27e2896ec31b1b0aea4cd4a90f7
Daniel Lopez <daniel.lopez09@telusinternational.com>
2023-02-26 13:40:55 -0600

    exercise

---
+++ exercises/ArrayMethod.js
@@ -0,0 +1,18 @@
+
+/**
+ * Add the missing logic to a transformToObjects() function that transforms 
+ * a list of numbers into a list of JavaScript objects.
+ * For the provided input [1, 2, 3] the transformToObjects() function should
+ *  return [{val: 1}, {val: 2}, {val: 3}]
+ */
+
+function transformToObjects(numberArray) {
+    let newArray = [];
+    
+    numberArray.forEach(el => {
+        let obj = {};
+        obj.val = el;
+        newArray.push(obj);
+    });
+    return newArray;
+}
